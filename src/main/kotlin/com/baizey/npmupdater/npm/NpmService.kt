18c990614b40ae51b831dd631ca13937d7b41269
Baizey <lassemoerk.mrk@gmail.com>
2023-04-04 06:21:13 +0200

    dont cache un-cacheable information

--- src/main/kotlin/com/baizey/npmupdater/npm/NpmService.kt
+++ src/main/kotlin/com/baizey/npmupdater/npm/NpmService.kt
@@ -54,41 +54,48 @@ class NpmService(
         fun isExpired() = System.nanoTime() > expireBy
     }
 
-    private val cache = concurrentMapOf<String, CacheItem<Dependency>>()
+    private val cache = concurrentMapOf<String, CacheItem<NpmVersions>>()
     private val mapper = ObjectMapper().registerKotlinModule()
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
             .configure(DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES, false)
 
-    fun getLatestVersion(dependency: PackageJsonDependency): Dependency {
-        val temp = cache.getOrDefault(dependency.name, null)
+    fun getLatestVersion(packageJsonDependency: PackageJsonDependency): Dependency {
+        val name = packageJsonDependency.name
+        val temp = cache.getOrDefault(name, null)
         if (temp != null && temp.isExpired()) {
             temp.extendExpiration()
-            thread.submit { cache.replace(dependency.name, fetchPackageInfo(dependency)) }
+            thread.submit { cache.replace(name, fetchPackageInfo(packageJsonDependency)) }
         }
-        val result = cache.computeIfAbsent(dependency.name) { fetchPackageInfo(dependency) }.value
-        result.index = dependency.index
-        return result
+        val cacheItem = cache.computeIfAbsent(name) { fetchPackageInfo(packageJsonDependency) }.value
+
+        val currentMatch = cacheItem.versions.firstOrNull { it == packageJsonDependency.current }
+        val current =
+                if (packageJsonDependency.current.isLatest) cacheItem.latest
+                else DependencyVersion.of(packageJsonDependency.current.versionWithType(), currentMatch?.deprecatedMessage)
+
+        return Dependency(
+                name,
+                packageJsonDependency.index,
+                current,
+                cacheItem.latest,
+                cacheItem.versions)
     }
 
-    private fun fetchPackageInfo(dependency: PackageJsonDependency): CacheItem<Dependency> {
-        val key = dependency.name
+    data class NpmVersions(val latest: DependencyVersion, val versions: List<DependencyVersion>)
+
+    private fun fetchPackageInfo(dependency: PackageJsonDependency): CacheItem<NpmVersions> {
         return try {
-            val jsonResponse = httpClient.get(URI(registry.url + key))
+            val jsonResponse = httpClient.get(URI(registry.url + dependency.name))
             val response = mapper.readValue<Response>(jsonResponse)
 
             val versions = response.versions.values.map { DependencyVersion.of(it.version, it.deprecated) }
 
-            val latestMatch = DependencyVersion.of(response.distTags.latest.trim(), null)
-            val latest = versions.first { it == latestMatch }
+            val latestVersion = DependencyVersion.of(response.distTags.latest.trim(), null)
+            val latest = versions.first { it == latestVersion }
 
-            val currentMatch = versions.firstOrNull { it == dependency.current }
-            val current =
-                    if (dependency.current.isLatest) latest
-                    else DependencyVersion.of(dependency.current.versionWithType(), currentMatch?.deprecatedMessage)
-
-            CacheItem(Dependency(key, dependency.index, current, latest, versions))
+            CacheItem(NpmVersions(latest, versions))
         } catch (e: IOException) {
-            CacheItem(Dependency(key, dependency.index, dependency.current, dependency.current, listOf()))
+            CacheItem(NpmVersions(dependency.current, listOf()))
         }
     }
 }
